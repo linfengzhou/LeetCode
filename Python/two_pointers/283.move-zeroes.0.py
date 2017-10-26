e914b76297c1656012d9a3473237c7a5df299803
Linfeng Zhou <luke.zlf@gmail.com>
2017-10-26 15:56:26 -0700

    update two pointers solution

---
+++ Python/two_pointers/283.move-zeroes.0.py
@@ -0,0 +1,13 @@
+class Solution(object):
+
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        left, right = 0, 0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
--- Python/two_pointers/283.move-zeroes.py
+++ Python/two_pointers/283.move-zeroes.py
@@ -1,7 +1,22 @@
 class Solution(object):
+
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        
+        left, right = 0, 1
+        while left < len(nums) and right < len(nums):
+            while left < len(nums) - 1 and nums[left] != 0:
+                left += 1
+
+            right = left + 1
+            while right < len(nums) - 1 and nums[right] == 0:
+                right += 1
+
+            if left < len(nums) and right < len(nums):
+                temp = nums[left]
+                nums[left] = nums[right]
+                nums[right] = temp
+                left += 1
+                right += 1
