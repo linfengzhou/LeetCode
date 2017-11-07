cad419db6b45e9e351e65b44b0f7a9bc8914c889
Linfeng Zhou <luke.zlf@gmail.com>
2017-11-06 18:19:24 -0800

    update solutions

--- Python/unsorted/56.merge-intervals.py
+++ Python/unsorted/56.merge-intervals.py
@@ -1,12 +1,29 @@
 # Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+class Interval(object):
+
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
 
 class Solution(object):
+
     def merge(self, intervals):
         """
         :type intervals: List[Interval]
         :rtype: List[Interval]
         """
+        res = []
+        if not intervals:
+            return res
+        intervals = sorted(intervals, key=lambda x: x.start)
+        last_interval = None
+        for interval in intervals:
+            if not last_interval:
+                last_interval = interval
+            elif last_interval.end < interval.start:
+                res.append(last_interval)
+                last_interval = interval
+            elif last_interval.end < interval.end:
+                last_interval.end = interval.end
+        return res + [last_interval]
--- Python/unsorted/58.length-of-last-word.py
+++ Python/unsorted/58.length-of-last-word.py
@@ -1,7 +1,27 @@
 class Solution(object):
+
     def lengthOfLastWord(self, s):
         """
         :type s: str
         :rtype: int
         """
-        
+        if not s or len(s) == 0:
+            return 0
+        n_s = len(s)
+        i = len(s) - 1
+        total_len = 0
+        count_sp = 0
+        while i >= 0:
+            if s[i].isalpha():
+                total_len += 1
+            elif s[i] == ' ':
+                if total_len == 0:
+                    pass
+                else:
+                    return total_len
+            i -= 1
+        return total_len
+
+# if __name__ == '__main__':
+#     a = Solution()
+#     print(a.lengthOfLastWord('"a "'))
