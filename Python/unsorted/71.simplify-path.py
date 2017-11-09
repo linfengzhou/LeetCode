51838934f7ea37830f1dd03b66e31793672f4217
linfengzhou <luke.zlf@gmail.com>
2017-11-09 11:16:21 -0800

    update solution

--- Python/unsorted/71.simplify-path.py
+++ Python/unsorted/71.simplify-path.py
@@ -1,7 +1,41 @@
 class Solution(object):
+
     def simplifyPath(self, path):
         """
         :type path: str
         :rtype: str
         """
-        
+
+        cur = ''
+        stack = []
+        i = 0
+        while i < len(path):
+            if path[i] == '/':
+                if cur == '.':
+                    pass
+                elif cur == '':
+                    pass
+                elif cur == '..':
+                    if len(stack) != 0:
+                        stack.pop()
+                else:
+                    stack.append(cur)
+                cur = ''
+            else:
+                cur += path[i]
+            i += 1
+
+        if cur == '.':
+            pass
+        elif cur == '':
+            pass
+        elif cur == '..':
+            if len(stack) != 0:
+                stack.pop()
+        else:
+            stack.append(cur)
+        return '/' + '/'.join(stack)
+
+# if __name__ == '__main__':
+#     a = Solution()
+#     print(a.simplifyPath('/.../'))
